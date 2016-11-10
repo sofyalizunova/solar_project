504589bd1336c275aad1e295399a1deb4e945994
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-11-10 17:19:48 +0300

    Лови

--- solar_input.py
+++ solar_input.py
@@ -19,10 +19,14 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            if object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -43,8 +47,8 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    s=input(line)
 
-    pass  # FIXME: not done yet
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +65,7 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    s=input(line)
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -77,8 +81,8 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
+            print(out_file, s)
+            
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
--- solar_model.py
+++ solar_model.py
@@ -19,8 +19,8 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        body.Fx +=body.m*obj.m*gravitational_constant*(body.x-obj.x)/r/r/r
+        body.Fy +=body.m*obj.m*gravitational_constant*(body.y-obj.y)/r/r/r
 
 
 def move_space_object(body, dt):
@@ -32,9 +32,11 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
+    ay= body.Fy/body.m
+    body.x +=ax*dt*dt/2
     body.Vx += ax*dt
-    # FIXME: not done recalculation of y coordinate!
+    body.y+=ay*dt*dt/2
+    body.Vy+=ay*dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
